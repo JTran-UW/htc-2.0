175721ee9ed9ab3a955c074707aefc9060d285b3
ProfJAT <46096425+ProfJAT@users.noreply.github.com>
2021-07-11 16:12:17 -0700

    Changed API names

--- htc/users/models.py
+++ htc/users/models.py
@@ -9,10 +9,10 @@ class AccountManager(BaseUserManager):
             raise ValueError('Users must have an email address')
 
         user = self.model(
-            first_name = first_name,
-            last_name = last_name,
+            first = first_name,
+            last = last_name,
             email = self.normalize_email(email),
-            phone_number = phone_number,
+            phone = phone_number,
             rating = rating,
             bio = bio
         )
@@ -23,10 +23,10 @@ class AccountManager(BaseUserManager):
     
     def create_superuser(self, first_name, last_name, email, phone_number, password):
         user = self.create_user(
-            first_name = first_name,
-            last_name = last_name,
+            first = first_name,
+            last = last_name,
             email = self.normalize_email(email),
-            phone_number = phone_number,
+            phone = phone_number,
             rating = 0,
             bio = "",
             password = password
@@ -38,10 +38,10 @@ class AccountManager(BaseUserManager):
 
 class User(AbstractBaseUser):
     # User data
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first = models.CharField(max_length=50)
+    last = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=10, unique=True)
+    phone = models.CharField(max_length=10, unique=True)
     rating = models.IntegerField(default=0) # Needs to be restricted from 1-5 within form
     bio = models.CharField(max_length=1000)
 
@@ -54,12 +54,12 @@ class User(AbstractBaseUser):
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number', 'password']
+    REQUIRED_FIELDS = ['first', 'last', 'phone', 'password']
 
     objects = AccountManager()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first} {self.last}"
     
     def has_perm(self, perm, obj=None):
         return self.is_admin
