37bb2f48ec865810b9f8e5fa78109153e2e2704a
ProfJAT <46096425+ProfJAT@users.noreply.github.com>
2021-07-11 20:12:22 -0700

    Fixed.

--- htc/dashboard/admin.py
+++ htc/dashboard/admin.py
@@ -4,10 +4,10 @@ from .models import RideOffer, RideRequest
 # Register your models here.
 
 class OfferAdmin(admin.ModelAdmin):
-    readonly_fields = ("driver", "date_created", "date_to_end", "description")
+    readonly_fields = ("driver", "date_created", "vehicle_type", "location", "begin_date", "end_date", "pricing_model", "price", "max_load", "size", "description")
 
 class RequestAdmin(admin.ModelAdmin):
-    readonly_fields = ("business", "date_created", "date_to_end", "description")
+    readonly_fields = ("business", "date_created", "vehicle_type", "location", "begin_date", "end_date", "pricing_model", "price", "max_load", "size", "description")
 
 admin.site.register(RideOffer, OfferAdmin)
 admin.site.register(RideRequest, RequestAdmin)
--- htc/dashboard/forms.py
+++ htc/dashboard/forms.py
@@ -4,9 +4,9 @@ from .models import RideOffer, RideRequest
 class OfferForm(ModelForm):
     class Meta:
         model = RideOffer
-        fields = ["driver", "date_to_end", "description"]
+        fields = ["driver", "vehicle_type", "location", "begin_date", "end_date", "pricing_model", "price", "max_load", "size", "description"]
 
 class RequestForm(ModelForm):
     class Meta:
         model = RideRequest
-        fields = ["business", "date_to_end", "description"]
+        fields = ["business", "vehicle_type", "location", "begin_date", "end_date", "pricing_model", "price", "max_load", "size", "description"]
